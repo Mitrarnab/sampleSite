1fa5d0d86962db2e7ae6d0571cb1f55f8273d11f
mitrarnab <mitrarnab0@gmail.com>
2025-05-30 02:09:52 +0530

    initially added first page, with responsive view

---
+++ assets/script/main.js
@@ -0,0 +1,72 @@
+AOS.init();
+
+// active class and attribute change for navbar
+$('.navbar-nav1 .nav-link').click(function () {
+    $('.navbar-nav1 .active').removeClass('active').removeAttr("aria-current");;
+    $(this).addClass('active').attr("aria-current", "page");
+})
+
+// on click outside navbar, navbar will collapse
+$(document).click(function (event) {
+    var clickTarget = $(event.target);
+    var isNavbar = clickTarget.closest('.navbar').length > 0;
+
+    if (!isNavbar && $('.navbar-collapse').hasClass('show')) {
+        $('.navbar-toggler').click();
+    }
+});
+
+// landing page slider 
+$('.servicesSlider').slick({
+    fade: true,
+    dots: false,
+    slidesToShow: 1,
+    infinite: false,
+    slidesToScroll: 1,
+    nextArrow: $('.servicesSection .serviceArrow .next'),
+    prevArrow: $('.servicesSection .serviceArrow .prev'),
+});
+
+// counter
+$(document).ready(function () {
+    let countersAnimated = false;
+
+    $(window).scroll(function () {
+        // Check if the counters have entered the viewport
+        const $counters = $('.counter span');
+        const triggerPoint = $counters.first().offset().top - window.innerHeight;
+
+        if (!countersAnimated && $(window).scrollTop() > triggerPoint) {
+            $counters.each(function () {
+                const $this = $(this);
+                const countTo = $this.attr('data-count');
+
+                $({ countNum: 0 }).animate(
+                    { countNum: countTo },
+                    {
+                        duration: 2000,
+                        easing: 'swing',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            $this.text(this.countNum);
+                        }
+                    }
+                );
+            });
+
+            countersAnimated = true;
+        }
+    });
+});
+
+//testimonial
+$('.testimonialSliders').slick({
+    dots: false,
+    slidesToShow: 1,
+    infinite: false,
+    slidesToScroll: 1,
+    nextArrow: $('.testimonial .serviceArrow .next'),
+    prevArrow: $('.testimonial .serviceArrow .prev'),
+});
